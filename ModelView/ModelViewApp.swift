48e5db1429da9f21fc5420b545f14553d3b2cc31
raddevus <1radeutsch1@gmail.com>
2024-05-13 11:00:56 -0400

    Initial Commit

---
+++ ModelView/ModelViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModelViewApp.swift
+//  ModelView
+//
+//  Created by roger deutsch on 5/13/24.
+//
+
+import SwiftUI
+
+@main
+struct ModelViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
